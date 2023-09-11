481900eb15bf07f2c374e35080bdd1d4f28a95e4
QAJOSEP <papat7442@gmail.com>
2023-09-10 21:41:08 -0600

    microservices cqrs edd

---
+++ department-service/src/test/java/com/josepillado/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josepillado.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service-registry/src/test/java/com/josepillado/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josepillado.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
